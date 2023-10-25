bb91e4005ef317493ee3ce080c331242c0307e71
Poul Henriksen <POHE@zealand.dk>
2023-10-25 16:32:24 +0200

    Add project files.

---
+++ StarterTestWeek43/Musician.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarterTestWeek43
+{
+    public class Musician
+    {
+        private List<Instrument> _instruments;
+
+        //implementer instance fields og properties ud fra klassediagrammet
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private string _address;
+
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+
+        private int _experience;
+
+        public int Experience
+        {
+            get { return _experience; }
+            set { _experience = value; }
+        }
+
+        public Musician(string name, string address, int experience)
+        {
+            _instruments = new List<Instrument>();
+            _name = name;
+            _address = address;
+            _experience = experience;
+        }
+
+        /// <summary>
+        /// Metoden skal tilføje et instrument til listen 
+        /// Der må ikke forekomme to instrumenter med samme id
+        /// </summary>
+        /// <param name="intstrument"></param>
+        public bool AddInstrument(Instrument instrument)
+        {
+            // TODO: Opgave 8 
+            //Skriv sætninger der tilføjer et instrument til listen
+            //Der må ikke forekomme to instrumenter der har samme id
+            
+            return false;
+        }
+
+        public override string ToString()
+        {
+            // TODO: Opgave 9 
+            // Skriv sætninger der udskriver alle informationer om musikeren og alle instrumenter
+            
+            return "";
+        }
+    }
+}
---
+++ StarterTestWeek43/Program.cs
@@ -0,0 +1,140 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StarterTestWeek43;
+
+
+Console.WriteLine("------------------- Del 1 --------------------");
+
+Console.WriteLine("Opgave 1");
+
+//TODO: Opgave 1
+// Lav et For loop som udskriver tallene fra og med 15 til og med 25
+// skriv din kode her
+
+
+
+Console.WriteLine("\nSlut på opgave 1");
+
+
+Console.WriteLine("\nOpgave 2");
+//TODO: Opgave 2
+// Lav et WHILE loop som udskriver tallene fra og med 15 til og med 25
+// skriv din kode her
+
+
+
+Console.WriteLine("\nSlut på opgave 2");
+
+
+Console.WriteLine("\nOpgave 3");
+//TODO: Opgave 3
+// Lav et FOR loop som beregner summen af tallene fra og med 15 til og med 25
+
+int sum = 0;
+
+// skriv din kode her
+
+
+
+Console.WriteLine($"Summen af tallene fra og med 15 til og med 25 er : {sum}");
+Console.WriteLine("Slut på opgave 3");
+
+
+
+Console.WriteLine("\nOpgave 4");
+// TODO: Opgave 4
+// Lav et FOREACH loop som udskriver tallene i listOfInt
+
+
+List<int> listOfInt = new List<int>();
+listOfInt.Add(4);
+listOfInt.Add(12);
+listOfInt.Add(9);
+listOfInt.Add(5);
+listOfInt.Add(12);
+listOfInt.Add(2);
+
+// skriv din kode her
+
+
+
+Console.WriteLine("\nSlut opgave 4");
+
+Console.WriteLine("\nOpgave 5");
+// TODO: Opgave 5
+// Lav et FOR loop som beregner summen som af tallene i listOfInt
+
+int listSum = 0;
+
+
+// skriv din kode her
+
+
+
+Console.WriteLine($"Summen af tallene i listOfInt: {listSum}");
+
+Console.WriteLine("Slut opgave 5");
+
+Console.WriteLine("\nOpgave 6");
+// TODO: Opgave 6
+// Lav et loop som undersøger om tallet i variablen searchNumber findes i listOfInt
+// Hvis tallet i searchNumber findes skal det udskrives at det findes. 
+// Hvis tallet ikke findes skal det udskrives at searchNumber ikke findes
+
+
+int searchNumber = 5;
+
+// skriv din kode her
+
+
+
+Console.WriteLine("Slut opgave 6");
+
+
+Console.WriteLine("\nOpgave 7");
+// TODO: Opgave 7
+// Lav et loop som beregner hvor mange gange tallet i variablen aNumber findes i listOfInt
+// Antallet skal opsamles i variablen countOfaNumber
+
+
+int aNumber = 12;
+int countOfaNumber = 0;
+
+
+// skriv din kode her
+
+
+
+Console.WriteLine("Slut opgave 7");
+
+
+
+Console.WriteLine("------------------- Del 2 --------------------");
+
+Console.WriteLine("Welcome to Music-World!");
+
+Musician m1 = new Musician("Jimi Hendrix", "Guitar street 123", 10);
+
+Instrument i1 = new Instrument(1, "Guitar");
+Instrument i2 = new Instrument(2, "Tværfløjte");
+
+Console.WriteLine("\nOpgave 8");
+
+// TODO: Opgave 8
+// Lav sætninger i metoden AddInstrument
+bool addedInstrument1 = m1.AddInstrument(i1);
+if (addedInstrument1)
+{
+    Console.WriteLine($"Instrumentet id {i1.Id} navn {i1.Name} er tilføjet til musikeren");
+}
+bool addedInstrument2 = m1.AddInstrument(i2);
+if (addedInstrument2)
+{
+    Console.WriteLine($"Instrumentet id {i1.Id} navn {i1.Name} er tilføjet til musikeren");
+}
+Console.WriteLine("Slut opgave 8");
+
+Console.WriteLine("\nOpgave 9");
+// TODO: Opgave 9
+// Lav sætninger i metoden Tostring der udskriver alle oplysninger om musikeren og alle
+Console.WriteLine(m1.ToString());
+Console.WriteLine("Slut opgave 9");
